bc026da38b8f75a37abd0b8d45a0efa8c9269497
Mikhail Rodionov <rodionov.mikhail.a@gmail.com>
2017-11-07 15:07:50 +0400

    Initial commit.

---
+++ app/src/main/java/com/intelthings/intelthings/MainActivity.java
@@ -0,0 +1,13 @@
+package com.intelthings.intelthings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
